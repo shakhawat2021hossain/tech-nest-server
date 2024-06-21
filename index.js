20ceaa666c76e365d288cb9cefb6d01cd99567f1
Shakhawat <shakhawat2021hossain2021@gmail.com>
2024-06-21 16:02:54 +0600

    ok

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
+require('dotenv').config()
 const cors = require("cors");
 const jwt = require("jsonwebtoken")
+var cookieParser = require('cookie-parser')
+
 const port = process.env.PORT || 5000;
 
 
@@ -10,22 +13,30 @@ app.use(cors())
 app.use(express.json())
 
 
-
 function createToken(user) {
-  const token = jwt.sign(
-    { email: user.email },
-    'secret',
-    { expiresIn: '7d' }
-  );
+  const token = jwt.sign({ email: user.email }, 'secret', { expiresIn: '7d' })
   return token;
 }
+
+function verifyToken(req, res, next){
+  const authToken = req.headers.authorization.split(" ")[1];
+  const verify = jwt.verify(authToken, 'secret')
+  console.log(verify);
+  if(!verify?.email){
+    return res.send("you are not authorized");
+  }
+  next()
+}
+
+
+
 
 app.get('/', (req, res) => {
   res.send("hello server")
 })
 
 
-const uri = "mongodb+srv://shakhawat2021hossain2021:gZAkVHSbmdJ0tL6e@cluster0.jnuic7t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jnuic7t.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -38,11 +49,11 @@ const client = new MongoClient(uri, {
 
 const dbConnect = async () => {
   try {
-      client.connect();
-      console.log("Database Connected Successfully✅");
+    client.connect();
+    console.log("Database Connected Successfully✅");
 
   } catch (error) {
-      console.log(error.name, error.message);
+    console.log(error.name, error.message);
   }
 }
 dbConnect()
@@ -53,7 +64,7 @@ const laptops = client.db('productsDB').collection('laptops')
 
 
 //products
-app.post('/laptops', async (req, res) => {
+app.post('/laptops', verifyToken, async (req, res) => {
   const laptop = req.body;
 
   const result = await laptops.insertOne(laptop);
@@ -75,7 +86,7 @@ app.get('/laptops/:id', async (req, res) => {
   res.send(data)
 })
 
-app.patch("/laptops/:id", async (req, res) => {
+app.patch("/laptops/:id", verifyToken, async (req, res) => {
   const id = req.params.id
   const updatedData = req.body;
   const data = await laptops.updateOne(
@@ -87,7 +98,7 @@ app.patch("/laptops/:id", async (req, res) => {
 
 })
 
-app.delete("/laptops/:id", async (req, res) => {
+app.delete("/laptops/:id", verifyToken, async (req, res) => {
   const id = req.params.id;
   const data = await laptops.deleteOne(
     { _id: new ObjectId(id) }
@@ -97,30 +108,31 @@ app.delete("/laptops/:id", async (req, res) => {
 })
 
 //users
-// const users = client.db("usersDB").collection("users");
 app.post('/users', async (req, res) => {
   const user = req.body;
-  const token = createToken(user);
-
+  const token = createToken(user)
+  console.log(token);
   const isExist = await users.findOne({ email: user?.email })
   if (isExist?._id) {
     return res.send({
       status: "success",
-      message: "login success",
+      message: "Login Successful",
       token
     });
   }
   await users.insertOne(user);
-  res.send(token)
+  res.send({ token })
 })
 
 
-app.get('/users/get/:id', async (req, res) => {
-  const id = req.params.id
-  // console.log(id);
-  const data = await users.findOne({ _id: new ObjectId(id) })
-  res.send(data)
+app.get('/users', async (req, res) => {
+  const usersData = users.find()
+  const result = await usersData.toArray()
+  res.send(result)
 })
+
+
+
 app.patch("/users/get/:id", async (req, res) => {
   const id = req.params.id
   const updatedData = req.body;
@@ -132,7 +144,12 @@ app.patch("/users/get/:id", async (req, res) => {
   console.log(data);
 
 })
-
+app.get('/users/get/:id', async (req, res) => {
+  const id = req.params.id
+  // console.log(id);
+  const data = await users.findOne({ _id: new ObjectId(id) })
+  res.send(data)
+})
 app.get('/users/:email', async (req, res) => {
   const email = req.params.email
   // console.log(email);
